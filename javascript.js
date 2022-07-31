ed3bd5616ee61440fd98aefbc3a1d3a88024310f
Akshay <ahanooman0307@gmail.com>
2022-07-30 23:38:23 -0400

    very buggy but working calc

--- javascript.js
+++ javascript.js
@@ -5,6 +5,9 @@ const clear = document.querySelector("#clear");
 const plus = document.querySelector("#plus");
 const equal = document.querySelector("#equal");
 const operation = document.querySelector(".operation");
+const subtract = document.querySelector("#minus");
+const mult = document.querySelector("#times");
+const divided = document.querySelector("#divided");
 
 let number = 0;
 let number1 = 0;
@@ -15,15 +18,28 @@ let add = 0;
 let sub = 0;
 let divide = 0;
 let multiply = 0;
+let op = false;
 
 let clearChecker = 0;
 
 function clearScreen(){
     if(clearChecker == 0){ 
-    number = 0;
-    number1 = 0;
-    number2 = 0;
+        plus.style.cssText = "background-color: rgb(247,149,7); color: white;";
+        subtract.style.cssText = "background-color: rgb(247,149,7); color: white;";
+        mult.style.cssText = "background-color: rgb(247,149,7); color: white;";
+        divided.style.cssText = "background-color: rgb(247,149,7); color: white;";
+            number = 0;
+            number1 = 0;
+            number2 = 0;
+            sum = 0;
+            
+            add = 0;
+            sub = 0;
+            divide = 0;
+            multiply = 0;
+            op = false;
     display.textContent = number;
+
     }
     else if(clearChecker == 1)
     {
@@ -53,15 +69,129 @@ function showAddition(){
         add = 1;
         number = 0;
     }
+    if(number1 != 0){
+        op = true;
+    }
+    sub = 0;
+    multiply = 0;
+    divide = 0;
     //minus = 0;
-    //divide = 0; etc...
+    //divide = 0; etc...(actually i think this might have to go in the show results part in order for it to work properly)
 
     plus.style.cssText = "background-color: white; color: rgb(247,149,7)";
+    subtract.style.cssText = "background-color: rgb(247,149,7); color: white;";
+    mult.style.cssText = "background-color: rgb(247,149,7); color: white;";
+    divided.style.cssText = "background-color: rgb(247,149,7); color: white;";
+}
+
+function showSubtraction(){
+    sub++;
+    if(sub == 1)
+    {
+        number = 0;
+    }
+    if(sub == 2){
+        sum = number1 - number2;
+        display.textContent = sum;
+        number1 = sum;
+        number2 = 0;
+        sub = 1;
+        number = 0;
+    }
+    if(number1 != 0){
+        op = true;
+    }
+    add = 0;
+    multiply = 0;
+    divide = 0;
+
+    subtract.style.cssText = "background-color: white; color: rgb(247,149,7)";
+    plus.style.cssText = "background-color: rgb(247,149,7); color: white;"
+    divided.style.cssText = "background-color: rgb(247,149,7); color: white;"
+    mult.style.cssText = "background-color: rgb(247,149,7); color: white;"
+}
+
+function showMultiplication(){
+    multiply++;
+    if(multiply == 1)
+    {
+        number = 0;
+    }
+    if(multiply == 2){
+        sum = number1 * number2;
+        display.textContent = sum;
+        number1 = sum;
+        number2 = 0;
+        multiply = 1;
+        number = 0;
+    }
+    if(number1 != 0){
+        op = true;
+    }
+    add = 0;
+    sub = 0;
+    divide = 0;
+
+    mult.style.cssText = "background-color: white; color: rgb(247,149,7)";
+    plus.style.cssText = "background-color: rgb(247,149,7); color: white;";
+    subtract.style.cssText = "background-color: rgb(247,149,7); color: white;";
+    divided.style.cssText = "background-color: rgb(247,149,7); color: white;";
+}
+
+function showDivision(){
+    divide++;
+    if(divide == 1)
+    {
+        number = 0;
+    }
+    if(divide == 2){
+        sum = number1 / number2;
+        display.textContent = sum;
+        number1 = sum;
+        number2 = 0;
+        divide = 1;
+        number = 0;
+    }
+    if(number1 != 0){
+        op = true;
+    }
+    add = 0;
+    sub = 0;
+    multiply = 0;
+
+    divided.style.cssText = "background-color: white; color: rgb(247,149,7)";
+    plus.style.cssText = "background-color: rgb(247,149,7); color: white;";
+    subtract.style.cssText = "background-color: rgb(247,149,7); color: white;";
+    mult.style.cssText = "background-color: rgb(247,149,7); color: white;";
 }
 
 function showResult(){
     if(add == 1){
         sum = number1 + number2;
+        display.textContent = sum;
+        number1 = sum;
+        number2 = 0;
+        add = 1;
+        number = 0;
+    }
+    if(sub == 1){
+        sum = number1 - number2;
+        display.textContent = sum;
+        number1 = sum;
+        number2 = 0;
+        add = 1;
+        number = 0;
+    }
+    if(multiply == 1){
+        sum = number1 * number2;
+        display.textContent = sum;
+        number1 = sum;
+        number2 = 0;
+        add = 1;
+        number = 0;
+    }
+    if(divide == 1){
+        sum = number1 / number2;
         display.textContent = sum;
         number1 = sum;
         number2 = 0;
@@ -75,20 +205,23 @@ num.forEach((m) => {
 
     // and for each one we add a 'click' listener
     m.addEventListener('click', () => {
-        if(add == 0){ //could change add to operation variable and then use other functions to do the work therefore less code
+        if(op == false){ //could change add to operation variable and then use other functions to do the work therefore less code
             number += m.textContent;
               display.textContent = (parseInt(number))
               
               number1 = (parseInt(number));
         }
-        else if(add == 1){
+        else if(op == true){
             
             number += m.textContent;
               display.textContent = (parseInt(number));
               
               number2 = (parseInt(number));
         }
-        plus.style.cssText = "background-color: rgb(247,149,7); color: white;"
+        plus.style.cssText = "background-color: rgb(247,149,7); color: white;";
+        subtract.style.cssText = "background-color: rgb(247,149,7); color: white;";
+        mult.style.cssText = "background-color: rgb(247,149,7); color: white;";
+        divided.style.cssText = "background-color: rgb(247,149,7); color: white;";
         clearChecker = 1;
         clear.textContent = "C";
     });
@@ -96,5 +229,8 @@ num.forEach((m) => {
 
 clear.addEventListener('click', clearScreen);   
 plus.addEventListener('click', showAddition);
-equal.addEventListener('click', showResult)
+equal.addEventListener('click', showResult);
+subtract.addEventListener('click', showSubtraction);
+mult.addEventListener('click', showMultiplication);
+divided.addEventListener('click', showDivision);
 
