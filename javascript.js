5d0cec662c6e40281d7106df88a480a31df992f5
Akshay <ahanooman0307@gmail.com>
2022-07-31 01:41:32 -0400

    fixed a lot of bugs and added most operations. screen still buggy

--- javascript.js
+++ javascript.js
@@ -12,12 +12,13 @@ const divided = document.querySelector("#divided");
 let number = 0;
 let number1 = 0;
 let number2 = 0;
-let sum = 0;
+let result = 0;
 
 let add = 0;
 let sub = 0;
 let divide = 0;
 let multiply = 0;
+let pressed = false;
 let op = false;
 
 let clearChecker = 0;
@@ -31,7 +32,7 @@ function clearScreen(){
             number = 0;
             number1 = 0;
             number2 = 0;
-            sum = 0;
+            result = 0;
             
             add = 0;
             sub = 0;
@@ -47,6 +48,19 @@ function clearScreen(){
         number2 = 0;
         display.textContent = number;
         clearChecker = 0;
+         
+        if(add == 1){
+            plus.style.cssText = "background-color: white; color: rgb(247,149,7)";
+        }
+        if(multiply ==1){
+            mult.style.cssText = "background-color: white; color: rgb(247,149,7)";
+        }
+        if(divide == 1){
+            divided.style.cssText = "background-color: white; color: rgb(247,149,7)";
+        }
+        if(sub == 1){
+            subtract.style.cssText = "background-color: white; color: rgb(247,149,7)";
+        }
     }
     clear.textContent = "AC";
 
@@ -62,9 +76,9 @@ function showAddition(){
         number = 0;
     }
     if(add == 2){
-        sum = number1 + number2;
-        display.textContent = sum;
-        number1 = sum;
+        result = number1 + number2;
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
         add = 1;
         number = 0;
@@ -91,9 +105,10 @@ function showSubtraction(){
         number = 0;
     }
     if(sub == 2){
-        sum = number1 - number2;
-        display.textContent = sum;
-        number1 = sum;
+        result = number1 - number2;
+        
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
         sub = 1;
         number = 0;
@@ -113,18 +128,21 @@ function showSubtraction(){
 
 function showMultiplication(){
     multiply++;
+    
     if(multiply == 1)
     {
         number = 0;
+        pressed = false;
     }
-    if(multiply == 2){
-        sum = number1 * number2;
-        display.textContent = sum;
-        number1 = sum;
+    if(multiply == 2 && pressed == true){
+        result = number1 * number2;
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
         multiply = 1;
         number = 0;
     }
+    if(multiply==3){multiply = 2;}
     if(number1 != 0){
         op = true;
     }
@@ -140,14 +158,20 @@ function showMultiplication(){
 
 function showDivision(){
     divide++;
+   
     if(divide == 1)
     {
         number = 0;
+        pressed = false;
     }
-    if(divide == 2){
-        sum = number1 / number2;
-        display.textContent = sum;
-        number1 = sum;
+    else if(divide != 0 && pressed == true){
+
+    }
+    if(divide == 2 && pressed == true){
+        result = number1 / number2;
+        if(number2 == 0){result = "Error"};
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
         divide = 1;
         number = 0;
@@ -167,37 +191,45 @@ function showDivision(){
 
 function showResult(){
     if(add == 1){
-        sum = number1 + number2;
-        display.textContent = sum;
-        number1 = sum;
+        result = number1 + number2;
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
-        add = 1;
+        // add = 1;
         number = 0;
     }
     if(sub == 1){
-        sum = number1 - number2;
-        display.textContent = sum;
-        number1 = sum;
+        result = number1 - number2;
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
-        add = 1;
+        // add = 1;
         number = 0;
     }
-    if(multiply == 1){
-        sum = number1 * number2;
-        display.textContent = sum;
-        number1 = sum;
+    if(multiply == 1 || multiply == 2){
+        result = number1 * number2;
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
-        add = 1;
+        // add = 1;
         number = 0;
     }
-    if(divide == 1){
-        sum = number1 / number2;
-        display.textContent = sum;
-        number1 = sum;
+    if(divide == 1 || divide == 2){
+        result = number1 / number2;
+        if(number2 == 0){result = "Error"};
+        display.textContent = result;
+        number1 = result;
         number2 = 0;
-        add = 1;
+        // add = 1;
         number = 0;
     }
+    add = 0;
+    sub = 0;
+    divide = 0;
+    multiply = 0;
+    clearChecker = 0; //resets everything if clear is pressed
+    pressed = false;
+    op = false;
 }
 
 
@@ -224,6 +256,7 @@ num.forEach((m) => {
         divided.style.cssText = "background-color: rgb(247,149,7); color: white;";
         clearChecker = 1;
         clear.textContent = "C";
+        pressed =true;
     });
   });
 
